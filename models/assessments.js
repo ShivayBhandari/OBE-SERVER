c481548fbdea6c79c5b9113c3bbf643da53ba8a2
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2021-11-02 22:03:06 +0530

    Added Missing Attribures in schema

--- models/assessments.js
+++ models/assessments.js
@@ -4,6 +4,7 @@ const Model = mongoose.model;
 
 const QuestionSchema = new Schema({
   coCode: { type: String, required: true },               // CO1
+  bloomLevel: { type: String, required: true },
   questionNo: { type: String, required: true },           // Q1
   questionStatement: { type: String, required: true },    // a...z
   maximumMarks: { type: Number, required: true }          // 2 / 4 / 8
@@ -16,6 +17,7 @@ const assessmentSchema = new Schema({
   termName: { type: String },
   termNo: { type: Number },
   courseTitle: { type: String },
+  courseCode: { type: String },
   courseId: { type: String },
   assessmentType: { type: String }, // Written Test / Assignments / Quiz
   assessmentName: { type: String, required: true },
---
+++ models/attainment.js
@@ -0,0 +1,42 @@
+/**
+ * studentName
+ * urn
+ * crn
+ * courseId
+ * curriculumId
+ * termId
+ * 
+ * MST1-Q1-CO1
+ * MST1-Q2-CO2
+ * MST1-Q3-CO3
+ * MST1-Q4-CO4
+ * MST1-Q5-CO5
+ * MST1-Q6-CO6
+ * MST2-Q1-CO1
+ * MST2-Q2-CO2
+ * MST2-Q3-CO3
+ * MST2-Q4-CO4
+ * MST2-Q5-CO5
+ * MST2-Q6-CO6
+ * 
+ * name 
+ * 
+ * 
+ * [[MST1, Q1, CO1], [MST1, Q2, CO1], [MST1, Q3, CO1], ...]
+ * 
+ * 
+ * userId == Assigning Curriculum ==> userHashcodeID [ Add ]
+ * userId == Assigning Curriculum ==> userHashcodeID [ Pop ]
+ * 
+ * curriculumID ===> []
+ * login ==> iterate 
+ * 
+ * 
+ */
+
+
+/**
+ * 1 Course => on average 3 documents (MST1 (6), MST2 (6), Assignment1 (2), ...) -> on average 14 questions => 1 All Questions
+ * 
+ * 40 Students => 40 Documents
+ */
