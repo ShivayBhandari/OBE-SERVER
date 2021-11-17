2726e02a0d42269aa58339ee18b0bee6860e2157
Manjot Singh <singhmanjot98035@gmail.com>
2021-11-17 15:39:07 +0530

    route change in attainmentRoutes.js

--- routes/attainmentRoutes.js
+++ routes/attainmentRoutes.js
@@ -2,8 +2,18 @@ const attainmentRoutes = require("express").Router();
 const { StudentAttainment } = require("./../models/attainment");
 
 attainmentRoutes.get('/:courseId', async (req, res) => {
+  // For CO Attainment [CALCULATION...]
   const attainment = await StudentAttainment.find({ 
     'courseId': req.params.courseId
+  });
+  return res.status(200).json({ attainments: attainment }).end();
+});
+
+attainmentRoutes.get('/:courseId/student-marks/:assessmentId', async (req, res) => {
+  // Fetching Student Records of Particular Assessment
+  const attainment = await StudentAttainment.find({ 
+    'courseId': req.params.courseId,
+    'assessmentId': req.params.assessmentId
   });
   return res.status(200).json({ attainments: attainment }).end();
 });
