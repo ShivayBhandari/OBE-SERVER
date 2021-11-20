a815e8ce0c18d49e1cc7568d6ec5d8b089436c1b
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2021-11-20 23:49:59 +0530

    Total Direact Attainment Routes

--- routes/attainmentRoutes.js
+++ routes/attainmentRoutes.js
@@ -1,16 +1,25 @@
 const attainmentRoutes = require("express").Router();
-const { StudentAttainment } = require("./../models/attainment");
+const { CIA_Marks, ESE_Marks } = require("./../models/attainment");
 
+// Fetching All Marks of Internal and External
 attainmentRoutes.get('/:courseId', async (req, res) => {
-  const attainment = await StudentAttainment.find({ 
-    'courseId': req.params.courseId
+  const ciaMarks = await CIA_Marks.find({ 'courseId': req.params.courseId });
+  const eseMarks = await ESE_Marks.find({ 'courseId': req.params.courseId });
+  return res.status(200).json({ ciaMarks: ciaMarks, eseMarks: eseMarks }).end();
+});
+
+// For Viewing Marks of Student of Particular Assessment
+attainmentRoutes.get('/:courseId/:assessmentId', async (req, res) => {
+  const attainment = await ((req.query?.ciaBool === 'true') ? CIA_Marks : ESE_Marks).find({ 
+    'courseId': req.params.courseId,
+    'assessmentId': req.params.assessmentId
   });
   return res.status(200).json({ attainments: attainment }).end();
 });
 
-attainmentRoutes.post('/add-student-attainment', async (req, res) => {
+attainmentRoutes.post('/add-student-marks', async (req, res) => {
   const { data } = { ...req.body };
-  StudentAttainment.insertMany([ ...data ]) 
+  ((req.query?.ciaBool === 'true') ? CIA_Marks : ESE_Marks).insertMany([ ...data ]) 
     .then((response) => {
       return res.status(200).json({ response: response, error: null, message: 'Student Marks Imported Successfully' }).end();
     })
