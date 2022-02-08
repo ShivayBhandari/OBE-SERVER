0155df0cbc195f4fb8ac983edd809a110b9a4d64
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2022-02-08 22:29:12 +0530

    Removing previous survey response from database

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -16,12 +16,12 @@ surveyRoutes.get("/:courseId/survey-response/:surveyId", async (req, res) => {
 
 surveyRoutes.post("/add-survey", async (req, res) => {
     let { data } = { ...req.body };
-    console.log(data);
+
+    await Survey.remove({ "courseId": data[0].courseId });
     Survey.insertMany(data)
     .then((value) => {
         return res.status(200).json({ response: value, error: null, message: 'Students Survey Response Imported Successfully' }).end();
-    })
-    .catch((error) => {
+    }, (error) => {
         console.log(error);
         return res.status(500).json({ response: null, error: error, message: "Something Went Wrong!! Please Try Again..." }).end();
     });
